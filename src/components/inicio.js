21c5d2fdc8cfdd4ceb71455aab95b2b3e12df5b4
Adrianaaa7 <161406755+Adrianaaa7@users.noreply.github.com>
2024-07-16 22:04:24 -0600

    Add files via upload

---
+++ src/components/inicio.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const inicio = () => {
+  return (
+    <div className="container mt-5">
+      <h2>Bienvenido!</h2>
+      <p>Inicio de sesión exitoso. ¡Bienvenido!</p>
+      <img src="../images/avion1.jpg" className='img-fluid w-75'></img>
+    </div>
+  );
+};
+
+export default inicio;
+
